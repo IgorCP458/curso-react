22d8f2a5b28ba0108084593215a9bc655c08ce47
Igor Pedroso <pedrosoic@hotmail.com>
2025-01-14 20:10:42 -0300

    Primeiro upload do Projeto :)

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,15 @@
+import { PostCard } from "../PostCard"
+
+
+export const Posts = ({posts}) => {
+    return(
+        <div className='posts'>
+          {posts.map(post => (
+            <PostCard
+              post = {post}
+              key={post.id}
+            />
+          ))}
+        </div>
+    )
+}
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,86 @@
+import { Component } from 'react';
+
+import './styles.css';
+
+import { loadPosts } from '../../utils/load-posts';
+import { Posts } from '../../components/Posts';
+import { TextInput } from '../../components/TextInput';
+import { Button } from '../../components/Button';
+
+class Home extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    postsPerPage: 2,
+    page: 0,
+    searchValue: ''
+  }
+
+  async componentDidMount() {
+    await this.loadPosts()
+  }
+
+  loadPosts = async () => {
+    const {page, postsPerPage } = this.state
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage), 
+      allPosts: postsAndPhotos})
+  }
+
+  loadMorePosts = () => {
+    const {
+      allPosts, posts, page, postsPerPage
+    } = this.state
+    const nextPage = page + postsPerPage
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
+    posts.push(...nextPosts)
+    this.setState({posts, page: nextPage})
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  }
+
+  render() {
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ?
+      allPosts.filter(post => {
+        return post.title.toLowerCase().includes(
+          searchValue.toLowerCase()
+        );
+      })
+      : posts;
+
+    return(
+      <section className='container'>
+        <div className="search-container">
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        </div>
+
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}
+
+        {filteredPosts.length === 0 && (
+          <p>Nenhum post encontrado! =(</p>
+        )}
+
+        <div className="button-container">
+          {!searchValue && (
+            <Button
+              text="Load more posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Home;
